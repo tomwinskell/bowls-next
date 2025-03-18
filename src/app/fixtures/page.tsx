610ce7de962634f41f5bf442aebb1182ba7593b3
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-03-18 14:00:27 -0400

    Move code inside component.

--- src/app/fixtures/page.tsx
+++ src/app/fixtures/page.tsx
@@ -5,23 +5,22 @@ import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persist
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { FixtureList } from '../ui/components/FixtureList';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 15, // 5 minutes (prevents refetching)
-      gcTime: 1000 * 60 * 60 * 24, // 24 hours (keeps inactive data)
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      refetchOnMount: false,
-    },
-  },
-});
-
-const persister = createSyncStoragePersister({
-  storage: window.localStorage,
-});
-
 export default function FixturesPage() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 15, // 5 minutes (prevents refetching)
+        gcTime: 1000 * 60 * 60 * 24, // 24 hours (keeps inactive data)
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+        refetchOnMount: false,
+      },
+    },
+  });
+  
+  const persister = createSyncStoragePersister({
+    storage: localStorage,
+  });
   return (
     <PersistQueryClientProvider
       client={queryClient}
