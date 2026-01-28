295340e7cfcdb19ead728a4d03f3101862d57cbb
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2026-01-27 20:03:36 -0500

    [updates] - add news page to navigation and as page - add NewsList item to get news articles from Google sheet of articles

---
+++ src/app/news/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { NewsList } from '@/components/news.list/NewsList';
+
+export default function NewsPage() {
+  return <NewsList />;
+}
---
+++ src/components/news.list/NewsList.tsx
@@ -0,0 +1,91 @@
+import { useQuery } from '@tanstack/react-query';
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
+import Image from 'next/image';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
+type NewsArticle = {
+  key: string;
+  date: string;
+  title: string;
+  text: string;
+  image?: string;
+  linkText?: string;
+  link?: string;
+};
+
+function useNews() {
+  return useQuery({
+    queryKey: ['news'],
+    queryFn: async (): Promise<Array<NewsArticle>> => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/v1/getSheet?id=1ps_CcRfDeXYJs3p0KTRX_td0JoVYXPS-0XIUrISnr_M&range=Sheet1`,
+      );
+      return await response.json();
+    },
+  });
+}
+
+export function NewsList() {
+  const { status, data, error, isFetching } = useNews();
+  console.log(data);
+  return (
+    <>
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        News
+      </h2>
+      <>
+        {status === 'pending' ? (
+          'Loading...'
+        ) : status === 'error' ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <div className="flex flex-col gap-4">
+            {data.map((article) => (
+              <Card
+                key={article.key}
+                className={cn(
+                  article.image && 'pt-0',
+                  'relative mx-auto w-full',
+                )}
+              >
+                {article.image && (
+                  <div className="relative aspect-video w-full h-50">
+                    <div className="absolute inset-0 z-30 bg-blue-950/35" />
+                    <Image
+                      src={article.image}
+                      alt="Event cover"
+                      fill
+                      className="relative z-20 object-cover brightness-60 grayscale dark:brightness-40"
+                    />
+                  </div>
+                )}
+                <CardHeader>
+                  <CardTitle>{article.title}</CardTitle>
+                  <CardDescription>{article.text}</CardDescription>
+                </CardHeader>
+                {article.link && article.linkText && (
+                  <CardFooter>
+                    <Link href={article.link}>
+                      <Button className="w-full" type="button">
+                        {article.linkText}
+                      </Button>
+                    </Link>
+                  </CardFooter>
+                )}
+              </Card>
+            ))}
+            <div>{isFetching ? 'Background Updating...' : ' '}</div>
+          </div>
+        )}
+      </>
+    </>
+  );
+}
